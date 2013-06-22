9b1cf6866c4abfe1d6dc0626ecdacfa85468218d
Nicolas Frankel <nicolas@frankel.ch>
2013-06-22 17:14:38 +0200

    Added example for text field value UiDataSource

--- src/com/packtpub/learnvaadin/clara/ClaraBehavior.java
+++ src/com/packtpub/learnvaadin/clara/ClaraBehavior.java
@@ -1,8 +1,10 @@
 package com.packtpub.learnvaadin.clara;
 
-
+import org.vaadin.teemu.clara.binder.annotation.UiDataSource;
 import org.vaadin.teemu.clara.binder.annotation.UiHandler;
 
+import com.vaadin.data.Property;
+import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Notification;
 
@@ -12,5 +14,11 @@ public class ClaraBehavior {
 	public void handleSubmitButtonClick(ClickEvent event) {
 
 		Notification.show("You're authenticated into Twaattin");
+	}
+
+	@UiDataSource("pin")
+	public Property<String> getLabelProperty() {
+
+		return new ObjectProperty<String>("1111", String.class);
 	}
 }
